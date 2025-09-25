8767275499a928414f0c65320381c2333037fa47
Johanne Courtright <johannecourtright@gmail.com>
2025-09-25 17:18:15 -0400

    add to index media components

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 export { default as WidthControl } from './width-control.js';
 export { default as ColumnControl } from './column-control.js';
+export { default as MediaComponent } from './media-component.js';
+export { default as MediaSettings } from './media-settings.js';
